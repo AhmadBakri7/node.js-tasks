1ce16b489b9073662e714c152c486e88af7b61e5
Ahmad Bakri <140534294+AhmadBakri7@users.noreply.github.com>
2024-09-13 19:18:23 +0300

    Add files via upload

--- task3/app.js
+++ task3/app.js
@@ -1,1 +1,45 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs").promises;
+const path = require("path");
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "rbakri9@gmail.com",
+    pass: "App Password",
+  },
+});
+
+async function sendEmail() {
+  try {
+    // Load Excel file
+    const excelFilePath = path.join(__dirname, "output_async.xlsx");
+    const excelFileContent = await fs.readFile(excelFilePath);
+
+    // Load the HTML content
+    const htmlFilePath = path.join(__dirname, "output.html");
+    const htmlContent = await fs.readFile(htmlFilePath, "utf8");
+
+    // Set up email data
+    const mailOptions = {
+      from: "rbakri9@gmail.com", // sender address
+      to: "rbakri9@gmail.com", // list of receivers
+      subject: "The excel and html", // Subject line
+      html: htmlContent, // html body
+      attachments: [
+        {
+          filename: "output_async.xlsx",
+          content: excelFileContent,
+        },
+      ],
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully!");
+  } catch (err) {
+    console.error("Error sending email:", err);
+  }
+}
+
+sendEmail();
